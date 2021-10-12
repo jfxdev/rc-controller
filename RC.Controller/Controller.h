a262c94d9f2ee89d4c8c9b5f9f55f4a5a49fb758
jfxdev <jffssd@gmail.com>
2021-10-11 23:18:59 -0300

    add read sync

--- RC.Controller/Controller.h
+++ RC.Controller/Controller.h
@@ -3,11 +3,29 @@
 
 #include "Arduino.h"
 
+struct Command {
+  int  L_X;
+  bool L_SW;
+  int  R_Y;
+  bool R_SW;
+  bool A_Button;
+  bool B_Button;
+}; 
+
 class Controller
 {
   public:
-    Controller();
+    Controller(byte LVRX_Pin, byte LSW_Pin, byte RVRY_Pin, byte RSW_Pin, byte A_Pin, byte B_Pin);
+    void setup();
+    void Read(Command *cmd);
   private:
+    Command _command;
+    byte _LVRX_Pin;
+    byte _LSW_Pin;
+    byte _RVRY_Pin;
+    byte _RSW_Pin;
+    byte _A_Pin;
+    byte _B_Pin;
 };
 
 #endif
